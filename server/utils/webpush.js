ef27be32bac66567daa698cadb6fd11372fa4d41
goutham <uppinurigouthamreddy@gmail.com>
2025-06-27 14:15:32 +0530

    SNS-2

--- server/utils/webpush.js
+++ server/utils/webpush.js
@@ -1,10 +1,11 @@
-import webPush from "web-push";
-const { VAPID_PRIVATE_KEY , VAPID_PUBLIC_KEY , VAPID_EMAIL } = process.env;
+
+const webPush = require("web-push");
+const { VAPID_PRIVATE_KEY, VAPID_PUBLIC_KEY, VAPID_EMAIL } = process.env;
 
 webPush.setVapidDetails(
   VAPID_EMAIL,
-  VAPID_PUBLIC_KEY ,
+  VAPID_PUBLIC_KEY,
   VAPID_PRIVATE_KEY
 );
 
-export default webPush;
+module.exports = webPush;
